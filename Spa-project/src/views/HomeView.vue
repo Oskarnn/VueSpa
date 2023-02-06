900f0a048c81b15c45a5620664837a12c6a278fb
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-02-06 11:02:24 +0100

    cards

--- Spa-project/src/views/HomeView.vue
+++ Spa-project/src/views/HomeView.vue
@@ -4,11 +4,40 @@
 </script>
 
 <template>
+    <!-- Lägger in komponent HeroImage -->
     <HeroImage />
-
-    <div class="container text-center">
-        <div class="row align-items-start">
-            <SpaOffers />
+    <!-- Textinterpolering -->
+    <h1 class="title">{{ message }}</h1>
+    <div id="offers">
+        <div class="container text-center">
+            <div class="row align-items-start">
+                <SpaOffers />
+                <SpaOffers />
+                <SpaOffers />
+            </div>
         </div>
     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Vilket paket önskas?'
+            }
+        }
+    }
+</script>
+<style scoped>
+    #offers {
+        display: flex;
+        justify-content: center;
+        margin-top: 10vh;
+        margin-bottom: 10vh;
+    }
+    .title,
+    h1 {
+        display: flex;
+        justify-content: center;
+        margin-top: 10vh;
+    }
+</style>
