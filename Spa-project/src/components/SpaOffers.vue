f1d6f6fc8b230402ec6b093d8f64faaa09234ae7
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-02-06 10:51:00 +0100

    blabla

---
+++ Spa-project/src/components/SpaOffers.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- Bootstrap card -->
+    <div class="col">
+        <div class="card" style="width: 18rem">
+            <img
+                src="../assets/img/laura-chouette-bD4j1V52dx0-unsplash.jpg"
+                class="card-img-top"
+                alt="bath"
+            />
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script></script>
+<style></style>
--- Spa-project/src/views/HomeView.vue
+++ Spa-project/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup>
-import HeroImage from '../components/HeroImage.vue'
+    import HeroImage from '../components/HeroImage.vue'
+    import SpaOffers from '../components/SpaOffers.vue'
 </script>
 
 <template>
-<HeroImage/>
+    <HeroImage />
+
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <SpaOffers />
+        </div>
+    </div>
 </template>
